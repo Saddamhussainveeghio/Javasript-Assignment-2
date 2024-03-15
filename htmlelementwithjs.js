e84d6b73cae6c16fdda3b7e2ae37422c8fbd22ee
Saddam Hussain <71175864+Saddamhussainveeghio@users.noreply.github.com>
2024-03-15 23:24:08 +0500

    completed 7th task

---
+++ htmlelementwithjs.js
@@ -0,0 +1,4 @@
+ let heading = document.createElement("h1");
+ heading.textContent = "This is a dynamically created heading.";
+ let container = document.getElementById("headingContainer");
+ container.appendChild(heading);
